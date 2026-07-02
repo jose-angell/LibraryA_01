42f6c4cf816054bdc50a487488cd9847f987136f
jose Angel <josegallardo.cor@gmail.com>
2026-07-01 23:48:58 -0600

    feat: se agrego el archivo de gitignore

--- LibraryA_01/obj/Debug/net10.0/LibraryA_01.AssemblyInfo.cs
+++ LibraryA_01/obj/Debug/net10.0/LibraryA_01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryA_01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3ba41ac1a18e1b572cb5cf5586b7f2b1701a236")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryA_01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryA_01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
