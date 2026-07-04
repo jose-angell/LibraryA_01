e0eacd5b69d62145e8c2e1314b21889789d50f0a
jose Angel <josegallardo.cor@gmail.com>
2026-07-03 23:27:41 -0600

    feat: configuracion de dbContext

---
+++ LibraryA_01/Infrastructure/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using LibraryA_01.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryA_01.Infrastructure
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Book> Books => Set<Book>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
