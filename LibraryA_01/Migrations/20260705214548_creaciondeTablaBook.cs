4e377c890c36b928d6db6422d82fd0bd791da76f
jose Angel <josegallardo.cor@gmail.com>
2026-07-05 15:52:03 -0600

    Feat: creacion de la migracion para la creacion de  la tabla de libro

---
+++ LibraryA_01/Migrations/20260705214548_creaciondeTablaBook.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryA_01.Migrations
+{
+    /// <inheritdoc />
+    public partial class creaciondeTablaBook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Author = table.Column<string>(type: "text", nullable: false),
+                    ISBN = table.Column<string>(type: "text", nullable: false),
+                    Year = table.Column<int>(type: "integer", nullable: false),
+                    NumberOfPages = table.Column<int>(type: "integer", nullable: false),
+                    Available = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
--- LibraryA_01/Program.cs
+++ LibraryA_01/Program.cs
@@ -1,10 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using LibraryA_01.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer(); 
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
+// Register AppDbContext using the DefaultConnection from appsettings.json
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
