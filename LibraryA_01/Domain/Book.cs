6b08bc6049faf98679ee34da7f70297e9b7e4b3a
jose Angel <josegallardo.cor@gmail.com>
2026-07-06 23:39:45 -0600

    feat: ajustes en el dominio y sus encapsulamiento

--- LibraryA_01/Domain/Book.cs
+++ LibraryA_01/Domain/Book.cs
@@ -1,16 +1,18 @@
-﻿namespace LibraryA_01.Domain
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryA_01.Domain
 {
     public class Book
     {
         public Guid Id { get; set; }
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public string ISBN { get; set; }
+        public string Title { get; private set; }
+        public string Author { get; private set; }
+        public string ISBN { get; private set; }
         public int Year { get; set; }
         public int NumberOfPages { get; set; }
         public bool Available { get; set; }
         
-        public Book()
+        private Book()
         {
             Title = string.Empty;
             Author = string.Empty;
@@ -19,18 +21,7 @@
         }
         public Book(string title, string author, string isbn, int year, int numberOfPages, bool available)
         {
-            if(string.IsNullOrWhiteSpace(title))
-            {
-                throw new ArgumentException("Title cannot be null or empty.", nameof(title));
-            }
-            if(string.IsNullOrWhiteSpace(author))
-            {
-                throw new ArgumentException("Author cannot be null or empty.", nameof(author));
-            }
-            if(string.IsNullOrWhiteSpace(isbn))
-            {
-                throw new ArgumentException("ISBN cannot be null or empty.", nameof(isbn));
-            }
+            var isValid = validate(title, author, isbn);
             Id = Guid.NewGuid();
             Title = title;
             Author = author;
@@ -40,6 +31,16 @@
             Available = available;
         }
         public void Update(string title, string author, string isbn, int year, int numberOfPages, bool available)
+        {
+            var isValid = validate(title, author, isbn);
+            Title = title;
+            Author = author;
+            ISBN = isbn;
+            Year = year;
+            NumberOfPages = numberOfPages;
+            Available = available;
+        }
+        private bool validate(string title, string author, string isbn)
         {
             if (string.IsNullOrWhiteSpace(title))
             {
@@ -53,12 +54,7 @@
             {
                 throw new ArgumentException("ISBN cannot be null or empty.", nameof(isbn));
             }
-            Title = title;
-            Author = author;
-            ISBN = isbn;
-            Year = year;
-            NumberOfPages = numberOfPages;
-            Available = available;
+            return true;
         }
     }
 }
